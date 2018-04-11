d9d9a6e21839be58524c2b41bdc01f5ef1422f64
nityanandagohain <nityanandagohain@gmail.com>
2018-04-11 23:57:17 +0530

    Files handled properly with notifications for error

--- src/app/sound-haven/player/player.component.ts
+++ src/app/sound-haven/player/player.component.ts
@@ -8,19 +8,29 @@ import { Howl } from 'howler';
   styleUrls: ['./player.component.css']
 })
 export class PlayerComponent implements OnInit {
-  public songSelected: boolean = false;
   public song: Howl;
-  private play = false;
+
+  public trackPlaying: boolean = false;
+  public trackListVisible: boolean = false;
+  public timer: string = "0.00";
+  public trackName: string;
+  public trackList: any = []; //file: && howl :
+
+  private currentIndex:number = 0; //Index of the track which is playing
+
   constructor(private _electronService: ElectronService, private chRef: ChangeDetectorRef) {
 
       const ipc = this._electronService.ipcRenderer;
 
       ipc.on('mp3-file', (event, arg) => {
           console.log(arg);
-          this.songSelected = true;
-          this.song = new Howl({
-            src: [arg]
-          })
+          this.trackPlaying = true;
+          // this.song = new Howl({
+          //   src: [arg[0]]
+          // })
+          for(let i=0; i< arg.length; i++){
+            this.trackList.push({file: arg[i], howl: null});
+          }
           this.chRef.detectChanges();
       });
 
@@ -29,6 +39,22 @@ export class PlayerComponent implements OnInit {
 
   }
   playSong(){
+    console.log('Play called');
+    this.play();
+  }
+
+  private play = () => {
+    if(this.trackList[this.currentIndex].howl == null){
+      this.song = this.trackList[this.currentIndex].howl = new Howl({
+        src: [this.trackList[this.currentIndex].file]
+      })
+    }else{
+      this.song = this.trackList[this.currentIndex].howl;
+    }
+
     this.song.play();
   }
+
+
+
 }
