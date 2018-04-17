c38ee970760bc0d5c576753c894c94701d0f0997
Nityananda Gohain <nityanandagohain@gmail.com>
2018-04-17 20:00:48 +0530

    Proper handling of error while the track list is empty (#10)

--- src/app/sound-haven/player/player.component.ts
+++ src/app/sound-haven/player/player.component.ts
@@ -7,49 +7,55 @@ import { TrackService } from '../services/tracks/track-service.service';
   styleUrls: ['./player.component.css']
 })
 export class PlayerComponent implements OnInit {
-   
+
   public tDisplay: string = '0:00';
   public tDuration: string = '0:00';
   public trackName: string;
   public trackPlaying: boolean = false;
   public trackListVisible: boolean = false;
 
-  constructor(private _trackService: TrackService) {};
+  constructor(private _trackService: TrackService) { };
 
-  ngOnInit() {};
-  
-  public playSong(){
+  ngOnInit() { };
+
+  public playSong() {
     console.log('Play called');
-    this._trackService.play();
-    this.trackPlaying = true; 
-    this.display();
+    let x = this._trackService.play();
+    if (x != -1) {
+      this.trackPlaying = true;
+      this.display();
+    }
   }
 
-  public pauseSong(){
+  public pauseSong() {
     console.log('Pause called');
     this._trackService.pause();
     this.trackPlaying = false;
   }
 
-  public stopSong(){
+  public stopSong() {
     console.log('Stop called');
     this._trackService.stop();
     this.trackPlaying = false;
   }
 
-  public playNext(){
+  public playNext() {
     console.log("Next called");
-    this._trackService.next();
-    this.display();
+    let x = this._trackService.next();
+    if (x != -1) {
+      this.display();
+    }
   }
 
-  public playPrev(){
+  public playPrev() {
     console.log("Prev called");
-    this._trackService.prev();
-    this.display();
+    let x = this._trackService.prev();
+    if (x != -1) {
+      this.display();
+    }
   }
 
-  public shuffle(){
+  public shuffle() {
     this._trackService.stop();
     console.log("Shuffle called");
     this.trackPlaying = true;
@@ -58,12 +64,12 @@ export class PlayerComponent implements OnInit {
     this.display();
   }
 
-  public display(){
+  public display() {
     this.trackName = this._trackService.getTrackName();
-    this.tDuration= this._trackService.returnTotalDuration();
+    this.tDuration = this._trackService.returnTotalDuration();
     setInterval(() => {
       this.tDisplay = this._trackService.returnCurrentDuration();
-      this.tDuration= this._trackService.returnTotalDuration();
+      this.tDuration = this._trackService.returnTotalDuration();
     }, 1000);
   }
 }
