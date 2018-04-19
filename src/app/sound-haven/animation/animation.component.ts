6e02b5cb796b2e31357c23155abb8fd9bcd6efee
nityanandagohain <nityanandagohain@gmail.com>
2018-04-19 20:18:17 +0530

    Animation added while playing song

---
+++ src/app/sound-haven/animation/animation.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+
+@Component({
+  selector: 'app-animation',
+  templateUrl: './animation.component.html',
+  styleUrls: ['./animation.component.css']
+})
+export class AnimationComponent implements OnInit {
+  public disp1 = "";
+  public disp2 = "";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public toggle = () => {
+    if (this.disp1.length > 0) {
+      this.disp1 = "";
+      this.disp2 = ""
+    } else {
+      this.disp1 = "loader1";
+      this.disp2 = "loader2";
+    }
+  }
+
+}
